e71b8873f4dd410bf8256e18f9d31407d91f2d7e
DebarunMitra <debarun96mitra@gmail.com>
2019-09-08 17:46:25 +0530

    Version 1.0
    character choose, clear done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+let val='';
+function uniueChar(value)
+{
+  //console.log(value);
+  val=val+value;
+  document.getElementById('clickedChar').innerHTML=val;
+}
 $(document).ready(function(){
-  let timeCount;
+  let timeCount,point,qc=1,n=0,charSet;
   //console.log(puzQue);
+  charSet=puzQue[qc].charset.split(' ');
+  $("#charSetButton").empty();
+  $.each(charSet,function(k,v){
+    //console.log(v);
+    let btn='<input class="character-button" onclick="uniueChar(this.value)"  type="button" value="'+v+'">';
+    $('#charSetButton').append(btn);
+  });
+  $('.clear').click(function(){
+    $("#clickedChar").html('Guess The Word');
+  });
   let timeUpdate = function() {
     $('.t-sec').each(function() {
      timeCount = parseInt($(this).html());
