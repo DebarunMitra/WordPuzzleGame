80873056210ae043c254d8dfacf51113ffc78a2d
DebarunMitra <debarun96mitra@gmail.com>
2019-09-09 08:20:57 +0530

    Version 1.0

--- js/puzzleData.js
+++ js/puzzleData.js
@@ -1,5 +1,6 @@
 const puzQue=[
 {level:1,word:"LION",charset:"P N T O S L Z I",noh:5,h1:"It is a animal",h2:"It has four leg and long tail",h3:"He lives in the jungle",h4:"One movie is named on him",h5:"He known as KING of the jungle"},
-{level:2,word:"OWL",charset:"G W T O S L Z C",noh:5,h1:"It is a Bird",h2:"It has two leg and two eyes",h3:"He is special for his neck",h4:"He fly at night",h5:"His sound is 'HOOT'"}
+{level:2,word:"OWL",charset:"G W T O S L Z C",noh:5,h1:"It is a Bird",h2:"It has two leg and two eyes",h3:"He is special for his neck",h4:"He fly at night",h5:"His sound is 'HOOT'"},
+{level:3,word:"ROCK",charset:"J O T R S K Z C",noh:5,h1:"It is a person",h2:"We see him in action movies",h3:"He is a Wrestler",h4:"His name has 'D D J' leters",h5:"His ring name start with 'R'"}
 ];
 let colors=['Tomato','Orange','DodgerBlue','MediumSeaGreen','SlateBlue','Coral','Green','DarkGoldenRod','DarkCyan','IndigoCrimson','Olive'];
